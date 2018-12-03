1d4f046a2f9b2461f099cb1778bd074c82bf27a1
UnaCorda <42948661+UnaCorda@users.noreply.github.com>
2018-12-02 17:46:37 -0800

    RBF_update

--- code/RBF.py
+++ code/RBF.py
@@ -5,6 +5,10 @@ Created on Sat Dec  1 22:11:55 2018
 @author: hasee
 """
 
+import numpy as np
+
+#######RBF scratch####################
+
 kernel = 1.0 * RBF(length_scale=100.0, length_scale_bounds=(1e-2, 1e3)) \
     + WhiteKernel(noise_level=1, noise_level_bounds=(1e-10, 1e+1))
 gp = GaussianProcessRegressor(kernel=kernel,
@@ -14,11 +18,20 @@ kernel = 1.0 * RBF()
 gp = GaussianProcessRegressor(kernel=kernel,
                               alpha=0.0).fit(X, y)
 
+kernel=4.4**2 * RBF(length_scale=41.8)+ 3.27**2 * RBF(length_scale=180)]
 
-kernel = 1.0 * RBF(length_scale=48.0, length_scale_bounds=(1e-2, 1e3))
+
+kernel = 1.0 * RBF(length_scale=48, length_scale_bounds=(1e-1, 1e3))
+
+#######################################
+
+#Generating the sparse point
+
+tt_processed = tt[:,np.newaxis]
+
+t_sparse = np.arange(0,48,6)
+t_sparse_processed = t_sparse[:,np.newaxis]
 
 gp = GaussianProcessRegressor(kernel=kernel)
-
-gp.fit(t_indice_processed,X1[451,t_indice])
-
-Y=gp.predict(tt_processed,X1[451])
+gp.fit(t_sparse_processed,X1[451,t_sparse_processed*10])
+Y=gp.predict(tt_processed)
